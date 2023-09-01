e65d59f431376e969a5bd68f10b2b1c9c6c1536c
Moundier <garamoundier.developer@protonmail.com>
2023-09-01 09:58:43 -0300

    refactor: remove dispensable files!

--- all-in-one/repo.sh
+++
@@ -1,7 +0,0 @@
-echo "# empty" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/Moundier/empty.git
-git push -u origin main
