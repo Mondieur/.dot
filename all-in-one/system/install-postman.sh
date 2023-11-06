dede9d01a2f2bc9c6e1bdc540f5aeec320522dfc
Moundier <garamoundier.developer@protonmaill.com>
2023-11-06 09:20:57 -0300

    chore: mv ./addons ./system
    chore: create test dir!
    
    chore: mv ./workflow.md ./gitflow.md!

---
+++ all-in-one/system/install-postman.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Define the content to be appended to the file
+entry="[Desktop Entry]\nType=Application\nName=Postman\nIcon=/opt/Postman/app/resources/app/assets/icon.png\nExec=\"/opt/Postman/Postman\"\nComment=Postman Desktop App\nCategories=Development;Code;"
+
+# Specify the target file
+postman_desktop="/usr/share/applications/postman.desktop"
+
+# Append the content to the file
+echo -e "$entry" >> "$postman_desktop"
+
+# Display a message to confirm the operation
+echo "Content appended to $file_path"
+
+# Extract Postman
+cd ~/Downloads/
+tar -xzf postman-linux-x64.tar.gz
+sudo mv Postman /opt
+sudo ln -s /opt/Postman/Postman /usr/local/bin/postman
---
+++ all-in-one/system/pyenv.sh
@@ -0,0 +1,14 @@
+git clone https://github.com/pyenv/pyenv.git ~/.pyenv
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
+echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
+echo -e 'if command -v pyenv 1>/dev/null 2>&1; then\n  eval "$(pyenv init -)"\nfi' >> ~/.bashrc
+source ~/.bashrc
+
+# install with pyenv
+# pyenv install 3.9.15
+
+# set global pyenv
+# pyenv global 3.9.15
+
+# verify global
+# python --version
